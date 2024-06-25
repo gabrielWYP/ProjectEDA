3bae71283ab649a4fdca5dee43cdfe35ac48937b
Gabriel Walter Ysidro Palomino <135283161+gabrielWYP@users.noreply.github.com>
2024-06-24 21:20:12 -0500

    Add files via upload

---
+++ ProyectoNUEVO/src/logica/LogicaTramite/Administrador.java
@@ -0,0 +1,16 @@
+
+package logica.LogicaTramite;
+
+public class Administrador {
+    private String usuario;
+    private String contrasena;
+
+    public Administrador(String usuario, String contrasena) {
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public void crearDependencia() {
+        
+    }
+}
---
+++ ProyectoNUEVO/src/logica/LogicaTramite/Dependencia.java
@@ -0,0 +1,17 @@
+
+package logica.LogicaTramite;
+public class Dependencia {
+    private String nombre;
+
+    public Dependencia(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
